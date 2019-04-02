a6f56757d3a0841be1cfa63b56f5bc5c6156f541
nonAlgebraic <sine@sinewave.me>
2019-04-02 16:15:14 -0400

    fixed notifications not clearning

--- webview.js
+++ webview.js
@@ -1,6 +1,6 @@
 module.exports = Franz => {
   const getNotifications = () => {
-    Franz.setBadge(0, document.querySelector('.mail-status.unread') ? 1 : 0);
+    Franz.setBadge(0, document.querySelector('.mail-status.unread') ? 1 : null);
   };
 
   Franz.loop(getNotifications);
